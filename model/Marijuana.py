af4208d95a39e2eb4351039a658db9156fe4b7e7
M.Charattrakool <m.charattrakool@gmail.com>
2016-12-23 08:15:42 +0700

    remove marijuana

--- model/Marijuana.py
+++
@@ -1,7 +0,0 @@
-import arcade
-
-class Marijuana(arcade.sprite.Sprite):
-    def __init__(self,location):
-        super().__init__("./img/marijuana.png")
-        self.set_position(location,600)
-        self.change_y = -10
