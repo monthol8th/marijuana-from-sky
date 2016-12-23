e95b56a5e32aed72af35fa850ad09cd90dcfe875
M.Charattrakool <m.charattrakool@gmail.com>
2016-12-23 11:05:21 +0700

    move every model into model sub package

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 from .model import *
 from .Panel import Panel
 from .Drawer import Drawer
-from .Updater import Updater
+from .PlayerViewUpdater import PlayerViewUpdater
--- src/model/__init__.py
+++ src/model/__init__.py
@@ -3,6 +3,6 @@ from .Envi import Envi
 from .Item import Item
 from .Jewel import Jewel
 from .Clover import Clover
-from .PlayerViewUpdater import PlayerViewUpdater
+from .Updater import Updater
 
-__all__ = ["Player","Envi","Item","Jewel","Clover","PlayerViewUpdater"]
+__all__ = ["Player","Envi","Item","Jewel","Clover","Updater"]
